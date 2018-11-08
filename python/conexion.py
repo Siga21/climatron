2ade79295400874e3ef981e2ae6f31970ecc5544
jroca72 <jroca72@gmail.com>
2018-11-08 12:52:53 +0100

    toqueteando el python

--- python/conexion.py
+++ python/conexion.py
@@ -4,14 +4,17 @@
 import sqlite3
 from libreria import * 
 
-
-texto = "select nombre, apellidos from clinica_clientes"
+texto = """select nombre_sensor, clima_planta.nombre_planta 
+           from clima_sensor 
+           inner join clima_planta 
+           on clima_sensor.planta_id = clima_planta.id 
+           order by clima_sensor.planta_id"""
 registros = run_query(texto)
 
 for i in range(len(registros)):
     nombre = registros[i][0]
-    apellidos = registros[i][1]
-    print 'El cliente es  : ' +  concatena(apellidos, nombre)
+    planta = registros[i][1]
+    print "El sensor es  : " +  nombre + " Planta: " + planta 
 
 
 
